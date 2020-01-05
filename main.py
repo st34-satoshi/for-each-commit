2f54d1a33accb718cdce625b76ab41b835a31d38
satoshi-tanaka <st34.satoshi@gmail.com>
2020-01-05 17:00:18 +0900

    able to output total line of all files which include commit

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ import subprocess
 
 
 def main():
+    # TODO: if you want to change a branch.
+    # # checkout new branch. not to break commit
+    # subprocess.call(["git", "checkout", "for-each-commit"])
+    # subprocess.call(["git", "checkout", "-b", "for-each-commit"])
+
+    # create log list. the list has only commit hash
     res_b = subprocess.check_output(["git", "log", "--pretty=oneline"])
     res = str(res_b.decode())
-    print(res)
+    log_list = [s.split(" ")[0] for s in res.split("\n") if len(s) > 1]  # log is only commit hash. avoid last new line
+
+    # do command for each commit
+    command()
+
+
+def command():
+    # TODO implement: this is a example. implement a function what you want
+    # output number of all files line
+    # get files
+    files = subprocess.check_output(["git", "ls-files"]).decode()
+    file_list = [s for s in files.split("\n") if len(s) > 1]
+    total_line = 0
+    for file in file_list:
+        # add line number of this file.
+        result_wc = subprocess.check_output(["wc", file]).decode().split(' ')
+        for s in result_wc:
+            if len(s) > 0:
+                total_line += int(s)
+                break
+    print(total_line)
 
 
 if __name__ == '__main__':
